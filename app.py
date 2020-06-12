9250c07b3bc43d2bceec12bcc6e2313f301988a2
tatiana-delgadillo-mojix-com <tatiana.delgadillo@mojix.com>
2020-06-12 14:37:19 -0400

    Testing prometeus

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import logging
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 from healthcheck import HealthCheck, EnvironmentDump
+import pandas as pd
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)-15s %(name)s - %(levelname)s - %(message)s")
 
+LOG_FILENAME = 'aplication.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 app = Flask(__name__)
 # Prometheus metrics.
 metrics = PrometheusMetrics(app)
@@ -24,11 +27,26 @@ app.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
 
 @app.route('/')
 @metrics.counter('hits_endpoint_hello', 'Hits in the endpoint.',
-         labels={'endpoint': "hello"})
-def hello():
-  logging.info("This endpoint has been hit!")
-  return "Hello Rodrigo!"
+         labels={'endpoint': "Load_df"})
+def Load_df():
+  try:
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    logging.info("Dataframe successfuly loaded!")
+    return "Working..."
+  except ValueError:
+    print("Can't find a sensible root for value.")
+    return "Not worwking..."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5000'))
+        DEBUG = os.environ.get('DEBUG','True') == "True"
+    except ValueError:
+        DEBUG = True
+        PORT = 5000
+    app.logger.info("Starting Service")
+    print (HOST,PORT)
+    app.run(host=HOST,port=PORT,debug=DEBUG)
 
